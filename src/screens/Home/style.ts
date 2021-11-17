2b59678814508dcd000c6bac674d8abcfe80a008
박종호 <jong951005@gmail.com>
2021-11-18 02:17:27 +0900

    [#8] feat: Home Component 퍼블리싱

---
+++ src/screens/Home/style.ts
@@ -0,0 +1,65 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+`;
+
+export const IntroSection = styled.View`
+  position: relative;
+  height: 25%;
+  margin: 12% 4% 0 4%;
+`;
+
+export const Logo = styled.Image`
+  width: 124px;
+  height: 18px;
+  margin-bottom: 12%;
+`;
+
+export const InduceParagraph = styled.Text`
+  width: 156px;
+  font-weight: 700;
+  font-size: 30px;
+  line-height: 42px;
+  margin-bottom: 5%;
+`;
+
+export const WaitingParagraph = styled.Text`
+  font-size: 17px;
+  line-height: 27px;
+`;
+
+export const ProfileImage = styled.Image`
+  position: absolute;
+  right: 0;
+  bottom: 60px;
+
+  width: 63px;
+  height: 63px;
+`;
+
+export const Bold = styled.Text`
+  font-weight: 700;
+`;
+
+export const LaughGroup = styled.Image`
+  width: 100%;
+  height: 41%;
+  margin-top: 5%;
+`;
+
+export const UpperArrow = styled.Image`
+  position: absolute;
+  align-self: center;
+  bottom: 5%;
+
+  width: 12px;
+  height: 4px;
+`;
+
+export const SlideToPivoting = styled.Image`
+  height: 60px;
+  margin-top: 6%;
+`;
